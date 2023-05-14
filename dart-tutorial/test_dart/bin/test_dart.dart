3ec3f441c44533aba7f13e3357819df6788f2f0a
afalaudn <58113952+afalaudn@users.noreply.github.com>
2023-05-14 21:33:33 +0700

    remove

--- dart-tutorial/test_dart/bin/test_dart.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:io';
-
-void main(List<String> arguments) {
-  String input = stdin.readLineSync();
-  print(input);
-}
--- test_installasi_dart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:test_installasi_dart.dart' as test_installasi_dart;
-
-void main(List<String> arguments) {
-  print('Hello World: ${test_instalasi_dart.dart.calculate()}!');
-}
